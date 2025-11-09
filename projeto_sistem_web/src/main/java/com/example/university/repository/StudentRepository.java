46d8679a002b21fd0f20ed82977364219426fc31
gabriel1sousa <105893429+gabriel1sousa@users.noreply.github.com>
2025-11-09 17:31:51 -0300

    Delete projeto_sistem_web directory

--- projeto_sistem_web/src/main/java/com/example/university/repository/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.university.repository;
-
-import com.example.university.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {}
